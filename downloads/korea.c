c78ff465fadbacd518ac66bf47c4aae7c7e5d375
leeshaowei0716 <144992123+leeshaowei0716@users.noreply.github.com>
2023-12-19 10:52:26 +0000

    w12

--- downloads/korea.c
+++ downloads/korea.c
@@ -1,55 +1,43 @@
 #include <stdio.h>
 #include <gd.h>
-#include <stdlib.h>
-#include <math.h>
 
-void drawSouthKoreanFlag() {
-    int width = 300;   // 寬度
-    int height = 200;  // 高度
-    int centerX = width / 2; // 中心X座標
-    int centerY = height / 2; // 中心Y座標
-    int outerRadius = 50;   // 外圈半徑
-    int innerRadius = 20;   // 內圈半徑
-
-    // 創建一個 24 位的彩色圖片
-    gdImagePtr im = gdImageCreateTrueColor(width, height);
-
-    // 設置背景色為白色
-    int white = gdImageColorAllocate(im, 255, 255, 255);
-    gdImageFill(im, 0, 0, white);
-
-    // 設置藍色橢圓
-    int blue = gdImageColorAllocate(im, 0, 0, 255);
-    gdImageFilledEllipse(im, centerX, centerY, 2 * outerRadius, outerRadius, blue);
-
-    // 設置紅色橢圓
-    int red = gdImageColorAllocate(im, 255, 0, 0);
-    gdImageFilledEllipse(im, centerX, centerY, 2 * outerRadius, innerRadius, red);
-
-    // 設置太極圖案的顏色
-    int black = gdImageColorAllocate(im, 0, 0, 0);
-    int white2 = gdImageColorAllocate(im, 255, 255, 255);
-
-    // 畫太極圖案
-    gdImageFilledEllipse(im, centerX, centerY, 2 * outerRadius, 2 * outerRadius, black);
-    gdImageFilledEllipse(im, centerX, centerY, 2 * innerRadius, 2 * innerRadius, white2);
-
-    // 輸出圖片到檔案
-    FILE *output = fopen("south_korean_flag.png", "wb");
-    if (output == NULL) {
-        fprintf(stderr, "Unable to open file for writing.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    gdImagePng(im, output);
-
-    // 釋放內存
-    gdImageDestroy(im);
-    fclose(output);
-}
+void drawTaiChi(gdImagePtr img, int x, int y, int radius, int red, int green, int blue);
 
 int main() {
-    drawSouthKoreanFlag();
-    printf("South Korean flag created: south_korean_flag.png\n");
+    int width = 400;
+    int height = 400;
+
+    gdImagePtr img = gdImageCreateTrueColor(width, height);
+    int white = gdImageColorAllocate(img, 255, 255, 255);
+    int blue = gdImageColorAllocate(img, 0, 0, 255);
+    int red = gdImageColorAllocate(img, 255, 0, 0);
+
+    // Fill the background with white
+    gdImageFilledRectangle(img, 0, 0, width - 1, height - 1, white);
+
+    // Draw the Tai Chi symbol
+    drawTaiChi(img, width / 2, height / 2, 50, red, 0, 0);
+
+    FILE *outputFile = fopen("tai_chi.png", "wb");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Error opening the output file.\n");
+        return 1;
+    }
+
+    gdImagePngEx(img, outputFile, 9);
+    fclose(outputFile);
+    gdImageDestroy(img);
+
     return 0;
+}
+
+void drawTaiChi(gdImagePtr img, int x, int y, int radius, int red, int green, int blue) {
+    // Draw blue circle
+    gdImageFilledEllipse(img, x, y, radius * 2, radius * 2, blue);
+
+    // Draw red half of Tai Chi symbol
+    gdImageFilledArc(img, x, y, radius * 2, radius * 2, 90, 270, red, gdChord);
+
+    // Draw white half of Tai Chi symbol
+    gdImageFilledArc(img, x, y, radius * 2, radius * 2, 270, 450, 255, gdChord);
 }
