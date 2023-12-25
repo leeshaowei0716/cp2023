6365cfbf60d27852b39650343157399566da0003
leeshaowei0716 <144992123+leeshaowei0716@users.noreply.github.com>
2023-12-25 13:22:20 +0000

    w12

--- downloads/heiio.c
+++ downloads/heiio.c
@@ -2,13 +2,19 @@
 #include <gd.h>
 #include <math.h>
 
-// 定義座標結構
-typedef struct {
-    double x;
-    double y;
-} Point;
+// Declare the rotation function
+void rotateFilledPolygon(int x_orig, int y_orig, double rotation_ang, gdPoint *points, int num_points) {
+    int i;
+    double angle_rad = rotation_ang * M_PI / 180.0;
 
-void rotateFilledPolygon(gdImagePtr img, int centerX, int centerY, double angle, gdPoint *points, int numPoints, int color);
+    for (i = 0; i < num_points; i++) {
+        int x = points[i].x - x_orig;
+        int y = points[i].y - y_orig;
+
+        points[i].x = x_orig + (int)(x * cos(angle_rad) - y * sin(angle_rad));
+        points[i].y = y_orig + (int)(x * sin(angle_rad) + y * cos(angle_rad));
+    }
+}
 
 int main() {
     int width = 800;
@@ -17,25 +23,24 @@ int main() {
     gdImagePtr img = gdImageCreateTrueColor(width, height);
     gdImageAlphaBlending(img, 0);
 
+    FILE *outputFile = fopen("hellogd2.png", "wb");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Error opening the output file.\n");
+        return 1;
+    }
+
     int red = gdImageColorAllocate(img, 255, 0, 0);
     int blue = gdImageColorAllocate(img, 0, 0, 255);
     int black = gdImageColorAllocate(img, 0, 0, 0);
     int white = gdImageColorAllocate(img, 255, 255, 255);
 
-    // 長方形塗色
     gdImageFilledRectangle(img, 0, 0, width, height, white);
     gdImageFilledRectangle(img, 0, 0, (int)width / 4, (int)height / 4, blue);
 
-    // 橢圓形塗色
     gdImageFilledEllipse(img, (int)width * 3 / 4, (int)height / 4, (int)width / 4, (int)width / 4, red);
-
-    // 橢圓形畫線
     gdImageEllipse(img, (int)width * 3 / 4, (int)height * 3 / 4, (int)width / 4, (int)width / 4, red);
-
-    // 畫直線
     gdImageLine(img, (int)width / 2, (int)height / 2, (int)width / 2, (int)height / 2 + 100, blue);
 
-    // 多邊形畫線
     gdPoint points[4];
     points[0].x = (int)width / 4;
     points[0].y = (int)height * 3 / 4;
@@ -45,9 +50,13 @@ int main() {
     points[2].y = points[1].y + 100;
     points[3].x = points[2].x - 100;
     points[3].y = points[2].y;
-    gdImagePolygon(img, points, 4, black);
 
-    // 多邊形塗色
+    // Call the rotation function multiple times
+    for (int i = 0; i < 4; i++) {
+        rotateFilledPolygon((int)width / 4 + 50, (int)height * 3 / 4 + 50, 30.0, points, 4);
+        gdImagePolygon(img, points, 4, black);
+    }
+
     gdPoint points2[4];
     points2[0].x = (int)width / 3;
     points2[0].y = (int)height / 2;
@@ -57,53 +66,17 @@ int main() {
     points2[2].y = points2[1].y + 100;
     points2[3].x = points2[2].x - 150;
     points2[3].y = points2[2].y;
-    gdImageFilledPolygon(img, points2, 4, red);
 
-    // 旋轉多邊形並保存每次旋轉後的圖形
+    // Call the rotation function multiple times
     for (int i = 0; i < 12; i++) {
-        char filename[50];
-        sprintf(filename, "rotated_image_%02d.png", i + 1);
-
-        gdImagePtr rotatedImg = gdImageCreateTrueColor(width, height);
-        gdImageAlphaBlending(rotatedImg, 0);
-
-        // 複製原始圖形
-        gdImageCopy(rotatedImg, img, 0, 0, 0, 0, width, height);
-
-        // 旋轉多邊形
-        rotateFilledPolygon(rotatedImg, 500, 200, 30.0, points2, 4, red);
-
-        // 儲存圖形
-        FILE *outputFile = fopen(filename, "wb");
-        if (outputFile == NULL) {
-            fprintf(stderr, "Error opening the output file.\n");
-            return 1;
-        }
-        gdImagePngEx(rotatedImg, outputFile, 9);
-        fclose(outputFile);
-        gdImageDestroy(rotatedImg);
+        //rotateFilledPolygon((int)width / 3 + 50, (int)height / 2 + 50, 30.0, points2, 4);
+        rotateFilledPolygon(500, 200, 30.0, points2, 4);
+        gdImageFilledPolygon(img, points2, 4, red);
     }
 
-    // 釋放主圖形的記憶體
+    gdImagePngEx(img, outputFile, 9);
+    fclose(outputFile);
     gdImageDestroy(img);
 
     return 0;
-}
-
-void rotateFilledPolygon(gdImagePtr img, int centerX, int centerY, double angle, gdPoint *points, int numPoints, int color) {
-    float radian = (float)(angle * M_PI / 180.0);
-    float cosA = cos(radian);
-    float sinA = sin(radian);
-
-    gdPoint rotatedPoints[numPoints];
-
-    for (int i = 0; i < numPoints; i++) {
-        int translatedX = points[i].x - centerX;
-        int translatedY = points[i].y - centerY;
-
-        rotatedPoints[i].x = centerX + round(translatedX * cosA - translatedY * sinA);
-        rotatedPoints[i].y = centerY + round(translatedX * sinA + translatedY * cosA);
-    }
-
-    gdImageFilledPolygon(img, rotatedPoints, numPoints, color);
 }
