1e42bdfd4ad987f5d088e9061336618ae39ada5c
leeshaowei0716 <144992123+leeshaowei0716@users.noreply.github.com>
2023-12-25 07:17:04 +0000

    w15

--- downloads/gd_roc_flag.c
+++ downloads/gd_roc_flag.c
@@ -2,20 +2,29 @@
 #include <gd.h>
 #include <math.h>
 
+// 定義座標結構
+typedef struct {
+    double x;
+    double y;
+} Point;
+
 void draw_roc_flag(gdImagePtr img);
-void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int white, int red, int blue);
+void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int color);
+
+// 計算圓與線的交點
+void circleLineIntersection(double h, double k, double r, double x1, double y1, double x2, double y2);
 
 int main() {
-    // width 3: height 2
+    // 設定圖片寬度和高度
     int width = 1200;
-    int height = (int)(width*2.0 / 3.0);
+    int height = (int)(width * 2.0 / 3.0);
 
     gdImagePtr img = gdImageCreateTrueColor(width, height);
     gdImageAlphaBlending(img, 0);
 
     draw_roc_flag(img);
 
-    FILE *outputFile = fopen("./roc_flag.png", "wb");
+    FILE *outputFile = fopen("roc_flag_in_gd.png", "wb");
     if (outputFile == NULL) {
         fprintf(stderr, "Error opening the output file.\n");
         return 1;
@@ -30,53 +39,128 @@ void draw_roc_flag(gdImagePtr img) {
     int width = gdImageSX(img);
     int height = gdImageSY(img);
     int red, white, blue;
-    int center_x = (int)(width/4);
-    int center_y = (int)(height/4);
-    int sun_radius = (int)(width/8);
+    int center_x = (int)(width / 4);
+    int center_y = (int)(height / 4);
+    int sun_radius = (int)(width / 8);
+    int white_circle_dia = sun_radius;
+    int blue_circle_dia = white_circle_dia + white_circle_dia * 2 / 15;
+    red = gdImageColorAllocate(img, 255, 0, 0);
+    white = gdImageColorAllocate(img, 255, 255, 255);
+    blue = gdImageColorAllocate(img, 0, 0, 149);
 
-    // Colors for the flag
-    red = gdImageColorAllocate(img, 242, 0, 0); // Red color
-    white = gdImageColorAllocate(img, 255, 255, 255); // White stripes
-    blue = gdImageColorAllocate(img, 0, 41, 204); // Blue
-
-    // 繪製紅色矩形區域
     gdImageFilledRectangle(img, 0, 0, width, height, red);
+    gdImageFilledRectangle(img, 0, 0, (int)(width / 2.0), (int)(height / 2.0), blue);
 
-    // 繪製藍色矩形區域
-    gdImageFilledRectangle(img, 0, 0, (int)(width/2.0), (int)(height/2.0), blue);
+    draw_white_sun(img, center_x, center_y, sun_radius, white);
 
-    // 繪製太陽
-    draw_white_sun(img, center_x, center_y, sun_radius, white, red, blue);
+    gdImageFilledEllipse(img, center_x, center_y, blue_circle_dia, blue_circle_dia, blue);
+    gdImageFilledEllipse(img, center_x, center_y, white_circle_dia, white_circle_dia, white);
+
+    // 連接第二組ABED的白線
+    int ax = 429;
+    int ay = 125;
+    int bx = 279;
+    int by = 165;
+    int ex = 170;
+    int ey = 274;
+    int dx = 170;
+    int dy = 274;
+
+    // 設定線條寬度
+    gdImageSetThickness(img, 3);
+
+    gdImageLine(img, ax, ay, bx, by, white);
+    gdImageLine(img, bx, by, ex, ey, white);
+    gdImageLine(img, ex, ey, dx, dy, white);
+    gdImageLine(img, dx, dy, ax, ay, white);
+
+    // 新增連線 (ax, ay) 到 (dx, dy)
+    gdImageLine(img, ax, ay, dx, dy, white);
+
+    // 恢復預設線條寬度
+    gdImageSetThickness(img, 1);
+
+    // 利用一個藍色大圓與白色小圓畫出藍色環狀
+    gdImageFilledEllipse(img, center_x, center_y, blue_circle_dia, blue_circle_dia, blue);
+    gdImageFilledEllipse(img, center_x, center_y, white_circle_dia, white_circle_dia, white);
+
+    // 圓的參數
+    double circle_x = (int)(width / 4); // 圓心 x 座標
+    double circle_y = (int)(height / 4); // 圓心 y 座標
+    double radius = white_circle_dia + white_circle_dia * 2 / 15; // 圓半徑
+
+    // 兩點座標
+    double x3 = 170;
+    double y3 = 274;
+    double x4 = 279;
+    double y4 = 165;
+
+    // 計算圓與線段 (x3, y3) 到 (x4, y4) 的交點
+    circleLineIntersection(circle_x, circle_y, radius, x4, y4, x3, y3);
+
+    double x5 = 279;
+    double y5 = 165;
+    double x6 = 429;
+    double y6 = 125;
+
+    // 計算圓與線段 (x6, y6) 到 (x5, y5) 的交點
+    circleLineIntersection(circle_x, circle_y, radius, x6, y6, x5, y5);
 }
-void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int white, int red, int blue) {
-    float angle = 0;
-    int numRays = 12; // 光芒的數量
 
-    gdPoint points[3]; // 三個頂點的陣列
+void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int color) {
+    float deg = M_PI / 180;
+    float sr = sun_radius / tan(75 * deg);
+    int ax, ay, bx, by, dx, dy, ex, ey;
+    gdPoint points[4];
 
-    for (int i = 0; i < numRays; i++) {
-        angle = i * (2 * M_PI / numRays);
-        float x1 = center_x + cos(angle) * sun_radius;
-        float y1 = center_y + sin(angle) * sun_radius;
+    ax = center_x;
+    ay = center_y - sun_radius;
+    bx = center_x - sun_radius * tan(15 * deg);
+    by = center_y;
+    ex = center_x;
+    ey = center_y + sun_radius;
+    dx = center_x + sun_radius * tan(15 * deg);
+    dy = center_y;
 
-        // 調整兩個底邊頂點的位置
-      float x2 = center_x + cos(angle + 0.35) * (sun_radius * 0.5);
-      float y2 = center_y + sin(angle + 0.35) * (sun_radius * 0.5);
-      float x3 = center_x + cos(angle - 0.35) * (sun_radius * 0.5);
-      float y3 = center_y + sin(angle - 0.35) * (sun_radius * 0.5);
+    for (int i = 1; i <= 6; i++) {
+        points[0].x = ax + sun_radius * sin(30 * deg * i);
+        points[0].y = ay + sun_radius - sun_radius * cos(30 * deg * i);
 
-        // 設定多邊形的三個頂點
-        points[0].x = (int)x1;
-        points[0].y = (int)y1;
-        points[1].x = (int)x2;
-        points[1].y = (int)y2;
-        points[2].x = (int)x3;
-        points[2].y = (int)y3;
+        points[1].x = bx + sr - sr * cos(30 * deg * i);
+        points[1].y = by - sr * sin(30 * deg * i);
 
-        gdImageFilledPolygon(img, points, 3, white);
+        points[2].x = ex - sun_radius * sin(30 * deg * i);
+        points[2].y = ey - (sun_radius - sun_radius * cos(30 * deg * i));
+
+        points[3].x = dx - (sr - sr * cos(30 * deg * i));
+        points[3].y = dy + sr * sin(30 * deg * i);
+
+        gdImageFilledPolygon(img, points, 4, color);
+        gdImagePolygon(img, points, 4, color);
     }
-  //外圈
-  gdImageFilledEllipse(img, center_x, center_y, sun_radius * 1.2, sun_radius * 1.2, blue);
+}
 
-    // 繪製太陽內部
-    gdImageFilledEllipse(img, center_x, center_y, sun_radius * 1.1, sun_radius * 1.1, 
+void circleLineIntersection(double h, double k, double r, double x1, double y1, double x2, double y2) {
+    double m = (y2 - y1) / (x2 - x1);
+    double b = y1 - m * x1;
+    double A = 1 + pow(m, 2);
+    double B = 2 * (m * b - m * k - h);
+    double C = pow(k, 2) - pow(r, 2) + pow(h, 2) - 2 * b * k + pow(b, 2);
+
+    double discriminant = pow(B, 2) - 4 * A * C;
+    if (discriminant > 0) {
+        double x_intersect1 = (-B + sqrt(discriminant)) / (2 * A);
+        double y_intersect1 = m * x_intersect1 + b;
+        printf("交點: (%.2f, %.2f)\n", x_intersect1, y_intersect1);
+
+        double x_intersect2 = (-B - sqrt(discriminant)) / (2 * A);
+        double y_intersect2 = m * x_intersect2 + b;
+        printf("交點: (%.2f, %.2f)\n", x_intersect2, y_intersect2);
+    } else if (discriminant == 0) {
+        double x_intersect = -B / (2 * A);
+        double y_intersect = m * x_intersect + b;
+        printf("交點: (%.2f, %.2f)\n", x_intersect, y_intersect);
+    } else {
+        printf("No points.\n");
+    }
+}
--- downloads/heiio.c
+++ downloads/heiio.c
@@ -2,6 +2,14 @@
 #include <gd.h>
 #include <math.h>
 
+// 定義座標結構
+typedef struct {
+    double x;
+    double y;
+} Point;
+
+void rotateFilledPolygon(gdImagePtr img, int centerX, int centerY, double angle, gdPoint *points, int numPoints, int color);
+
 int main() {
     int width = 800;
     int height = 600;
@@ -9,32 +17,28 @@ int main() {
     gdImagePtr img = gdImageCreateTrueColor(width, height);
     gdImageAlphaBlending(img, 0);
 
-    FILE *outputFile = fopen("hellogd.png", "wb");
-    if (outputFile == NULL) {
-
-fprintf(stderr, "Error opening the output file.\n");
-
-return 1;
-    }
-
     int red = gdImageColorAllocate(img, 255, 0, 0);
     int blue = gdImageColorAllocate(img, 0, 0, 255);
     int black = gdImageColorAllocate(img, 0, 0, 0);
     int white = gdImageColorAllocate(img, 255, 255, 255);
+
     // 長方形塗色
     gdImageFilledRectangle(img, 0, 0, width, height, white);
-    gdImageFilledRectangle(img, 0, 0, (int)width/4, (int)height/4, blue);
+    gdImageFilledRectangle(img, 0, 0, (int)width / 4, (int)height / 4, blue);
+
     // 橢圓形塗色
-    gdImageFilledEllipse(img, (int)width*3/4, (int)height/4, (int)width/4, (int)width/4, red);
+    gdImageFilledEllipse(img, (int)width * 3 / 4, (int)height / 4, (int)width / 4, (int)width / 4, red);
+
     // 橢圓形畫線
-    gdImageEllipse(img, (int)width*3/4, (int)height*3/4, (int)width/4, (int)width/4, red);
+    gdImageEllipse(img, (int)width * 3 / 4, (int)height * 3 / 4, (int)width / 4, (int)width / 4, red);
+
     // 畫直線
-    gdImageLine(img, (int)width/2, (int)height/2, (int)width/2, (int)height/2 + 100, blue);
+    gdImageLine(img, (int)width / 2, (int)height / 2, (int)width / 2, (int)height / 2 + 100, blue);
 
     // 多邊形畫線
     gdPoint points[4];
-    points[0].x = (int)width/4;
-    points[0].y = (int)height*3/4;
+    points[0].x = (int)width / 4;
+    points[0].y = (int)height * 3 / 4;
     points[1].x = points[0].x + 100;
     points[1].y = points[0].y;
     points[2].x = points[1].x;
@@ -45,8 +49,8 @@ return 1;
 
     // 多邊形塗色
     gdPoint points2[4];
-    points2[0].x = (int)width/3;
-    points2[0].y = (int)height/2;
+    points2[0].x = (int)width / 3;
+    points2[0].y = (int)height / 2;
     points2[1].x = points2[0].x + 100;
     points2[1].y = points2[0].y;
     points2[2].x = points2[1].x;
@@ -55,8 +59,51 @@ return 1;
     points2[3].y = points2[2].y;
     gdImageFilledPolygon(img, points2, 4, red);
 
-    gdImagePngEx(img, outputFile, 9);
-    fclose(outputFile);
+    // 旋轉多邊形並保存每次旋轉後的圖形
+    for (int i = 0; i < 12; i++) {
+        char filename[50];
+        sprintf(filename, "rotated_image_%02d.png", i + 1);
+
+        gdImagePtr rotatedImg = gdImageCreateTrueColor(width, height);
+        gdImageAlphaBlending(rotatedImg, 0);
+
+        // 複製原始圖形
+        gdImageCopy(rotatedImg, img, 0, 0, 0, 0, width, height);
+
+        // 旋轉多邊形
+        rotateFilledPolygon(rotatedImg, 500, 200, 30.0, points2, 4, red);
+
+        // 儲存圖形
+        FILE *outputFile = fopen(filename, "wb");
+        if (outputFile == NULL) {
+            fprintf(stderr, "Error opening the output file.\n");
+            return 1;
+        }
+        gdImagePngEx(rotatedImg, outputFile, 9);
+        fclose(outputFile);
+        gdImageDestroy(rotatedImg);
+    }
+
+    // 釋放主圖形的記憶體
     gdImageDestroy(img);
+
     return 0;
+}
+
+void rotateFilledPolygon(gdImagePtr img, int centerX, int centerY, double angle, gdPoint *points, int numPoints, int color) {
+    float radian = (float)(angle * M_PI / 180.0);
+    float cosA = cos(radian);
+    float sinA = sin(radian);
+
+    gdPoint rotatedPoints[numPoints];
+
+    for (int i = 0; i < numPoints; i++) {
+        int translatedX = points[i].x - centerX;
+        int translatedY = points[i].y - centerY;
+
+        rotatedPoints[i].x = centerX + round(translatedX * cosA - translatedY * sinA);
+        rotatedPoints[i].y = centerY + round(translatedX * sinA + translatedY * cosA);
+    }
+
+    gdImageFilledPolygon(img, rotatedPoints, numPoints, color);
 }
